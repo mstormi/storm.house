355dcdedc4850e4a3804ffddd5fa0598042ec9c2
Markus Storm <markus.storm@gmx.net>
2021-10-23 23:38:04 +0200

    setup_inverter_config -> setup_pv_config
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -126,7 +126,7 @@ if [[ -n "$UNATTENDED" ]]; then
   exim_setup
   permissions_corrections
   setup_mirror_SD "install"
-  setup_inverter_config "${invertertype:-kostal}" "${inverterip:-192.168.178.100}"
+  setup_pv_config "${invertertype:-kostal}" "${inverterip:-192.168.178.100}"
   install_cleanup
 else
   apt_update
