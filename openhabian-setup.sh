0b22b13096604a125bd7fdb8016fb62c70897236
Markus Storm <markus.storm@gmx.net>
2022-06-19 21:30:12 +0200

    replace openHAB logo with storm.house logo
    Signed-off-by: Markus Storm <markus.storm@gmx.net>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -111,6 +111,7 @@ if [[ -n "$UNATTENDED" ]]; then
   firemotd_setup
   java_install "${java_opt:-17}"
   openhab_setup "${clonebranch:-openHAB}" "release"
+  replace_logo
   import_openhab_config
   openhab_shell_interfaces
   vim_openhab_syntax
