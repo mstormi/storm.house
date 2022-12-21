3e90e0cfb300dd0a6041b89b76551a3013df30c0
openhabian <openhabian@openHABian>
2022-12-21 15:02:37 +0100

    unnötiges wie frontail entfernt
    Signed-off-by: openhabian <openhabian@openHABian>

--- openhabian-setup.sh
+++ openhabian-setup.sh
@@ -121,9 +121,9 @@ if [[ -n "$UNATTENDED" ]]; then
   srv_bind_mounts
   samba_setup
   clean_config_userpw
-  frontail_setup
-  custom_frontail_log "add" "$custom_log_files"
-  jsscripting_npm_install "openhab_rules_tools"
+  #frontail_setup
+  #custom_frontail_log "add" "$custom_log_files"
+  #jsscripting_npm_install "openhab_rules_tools"
   zram_setup
   exim_setup
   permissions_corrections
