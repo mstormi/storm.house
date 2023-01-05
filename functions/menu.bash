8ec6eaf1c6dbd44c1df876fd17d764042814993e
openhabian <openhabian@openHABian>
2023-01-05 14:05:37 +0100

    update logo nach OH update
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/menu.bash
+++ functions/menu.bash
@@ -60,7 +60,6 @@ show_main_menu() {
   elif [[ "$choice" == "02"* ]]; then
     wait_for_apt_to_finish_update
     system_upgrade
-    #update_ems
     replace_logo
 
   elif [[ "$choice" == "03"* ]]; then
@@ -73,6 +72,8 @@ show_main_menu() {
     repo=$(apt-cache madison openhab | head -n 1 | awk '{ print $6 }' |cut -d'/' -f1)
     # shellcheck disable=SC2154
     openhab_setup "${repo:-release}" "${openhabpkgversion}"
+    update_ems
+    replace_logo
 
   elif [[ "$choice" == "04"* ]]; then
     import_openhab_config
