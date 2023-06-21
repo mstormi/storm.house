603f5bbf8cc7f4acbd067f9db0f87c5d8925f43f
openhabian <openhabian@openHABian>
2023-06-21 14:32:44 +0200

    install openhab-3.4.4-2
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/openhab.bash
+++ functions/openhab.bash
@@ -26,8 +26,7 @@ create_systemd_dependencies() {
 openhab_setup() {
   local introText
   local keyName="openhab"
-  local openhabVersion
-  local installVersion
+  local openhabVersion="3.4.4-2"
   local repo
   local successText
   local ohPkgName="openhab"
