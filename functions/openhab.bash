902ed5ff3f62211b79d8eb4792340f33e2e63f4d
openhabian <openhabian@openHABian>
2023-06-21 13:40:26 +0200

    install openhab-3.4.4-2
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/openhab.bash
+++ functions/openhab.bash
@@ -48,7 +48,7 @@ delayed_rules() {
 openhab_setup() {
   local introText
   local keyName="openhab"
-  local openhabVersion="3.4.2-1"
+  local openhabVersion="${3:-3.4.4-2}"
   local repo
   local successText
 
