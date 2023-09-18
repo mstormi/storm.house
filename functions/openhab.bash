44e10821b33455cb6d23b86ab28292af839a3390
openhabian <openhabian@openHABian>
2023-09-18 13:30:22 +0200

    pkg 4.0.3-1
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/openhab.bash
+++ functions/openhab.bash
@@ -26,7 +26,7 @@ create_systemd_dependencies() {
 openhab_setup() {
   local introText
   local keyName="openhab"
-  local openhabVersion="4.0.2-1"
+  local openhabVersion="4.0.3-1"
   local repo
   local successText
   local ohPkgName="openhab"
