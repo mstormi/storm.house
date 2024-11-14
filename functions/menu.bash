1e7da6e552bbdcd46a136f6f1fb175cdfc5df482
openhabian <openhabian@openHABian>
2024-11-14 14:30:28 +0100

    Option n bei Rückfrage während evcc-Upgrade
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/menu.bash
+++ functions/menu.bash
@@ -80,9 +80,8 @@ show_main_menu() {
     fi
     repo=$(apt-cache madison openhab | head -n 1 | awk '{ print $6 }' |cut -d'/' -f1)
     cond_redirect apt-mark unhold openhab openhab-addons evcc
-    #if ! cond_redirect apt install -y --allow-downgrades evcc="${evccForcePkg:-${evccpkgversion}}"; then echo "FAILED (EVCC package installation)"; return 1; fi
     ver="${evccForcePkg:-${evccpkgversion}}"
-    if ! cond_redirect apt install -y --allow-downgrades "evcc${ver+=$ver}"; then echo "FAILED (EVCC package installation)"; fi
+    if ! cond_redirect echo -n n | apt install -y --allow-downgrades "evcc${ver+=$ver}"; then echo "FAILED (EVCC package installation)"; fi
     openhab_setup "openHAB" "${repo:-release}" "${openhabForcePkg:-${openhabpkgversion:-4.2.2-1}}"
     upgrade_ems
     cond_redirect apt-mark hold openhab openhab-addons evcc
