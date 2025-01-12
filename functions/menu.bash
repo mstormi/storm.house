491dadc61efcf9f7d042d8f028a444da31c42d13
openhabian <openhabian@openHABian>
2025-01-12 19:55:53 +0100

    fix double openhab pkg install and missing logo if aborted
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/menu.bash
+++ functions/menu.bash
@@ -75,13 +75,11 @@ show_main_menu() {
     fi
 
     repo=$(apt-cache madison openhab | head -n 1 | awk '{ print $6 }' |cut -d'/' -f1)
-    # shellcheck disable=SC2154
-    openhab_setup "${repo:-release}" "${openhabpkgversion}"
     cond_redirect apt-mark unhold openhab openhab-addons evcc
-    openhab_setup "${repo:-release}"
+    openhab_setup "${repo:-release}" "${openhabpkgversion}"
+    replace_logo
     upgrade_ems
     cond_redirect apt-mark hold openhab openhab-addons evcc
-    replace_logo
 
   elif [[ "$choice" == "04"* ]]; then
     import_openhab_config
