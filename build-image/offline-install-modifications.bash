f91f1e8a0950d67a6fc4dcc05b0efd90c2c2cb38
openhabian <openhabian@openHABian>
2025-02-03 07:41:25 +0100

    build 21 nach ARM runners
    Signed-off-by: openhabian <openhabian@openHABian>

--- build-image/offline-install-modifications.bash
+++ build-image/offline-install-modifications.bash
@@ -4,6 +4,7 @@
 export BASEDIR="/opt/openhabian"
 export DEBIAN_FRONTEND="noninteractive"
 export PREOFFLINE="1"
+
 debfileurl=https://davesteele.github.io/comitup/deb
 debfile=davesteele-comitup-apt-source
 debfilelatest=latest.deb
