48d8304e33fbd509e0733930ab888a05489faec0
openhabian <openhabian@openHABian>
2025-03-01 15:12:13 +0100

    check OH5
    Signed-off-by: openhabian <openhabian@openHABian>

--- functions/menu.bash
+++ functions/menu.bash
@@ -81,13 +81,6 @@ show_main_menu() {
   elif [[ "$choice" == "04"* ]]; then
     import_openhab_config
 
-<<<<<<< HEAD
-=======
-  elif [[ "$choice" == "05"* ]]; then
-    setup_pv_config
-    setup_wb_config
-
->>>>>>> 8d17c5c5e (Wallbox in Einstellungen auswaehlbar machen)
   elif [[ "$choice" == "10"* ]]; then
     choice2=$(whiptail --title "storm.house Configuration Tool $(get_git_revision)" --menu "Apply Improvements" 13 116 6 --cancel-button Back --ok-button Execute \
     "11 | Packages"               "Install needed and recommended system packages" \
@@ -172,7 +165,6 @@ show_main_menu() {
     "32 | Set system locale"      "Change system language, currently '$(env | grep "^[[:space:]]*LANG=" | sed 's|LANG=||g')'" \
     "33 | Set system timezone"    "Change your timezone, execute if it's not '$(printf "%(%H:%M)T\\n" "-1")' now" \
     "34 | Change passwords"       "Change passwords for Samba, openHAB Console or the system user" \
-<<<<<<< HEAD
     "35 | Serial port"            "Prepare serial ports for peripherals like RaZberry, ZigBee adapters etc" \
     "36 | Disable framebuffer"    "Disable framebuffer on RPi to minimize memory usage" \
     "   | Enable framebuffer"     "Enable framebuffer (standard setting)" \
@@ -182,16 +174,6 @@ show_main_menu() {
     "39 | Move root to USB"       "Move the system root from the SD card to a USB device (SSD or stick)" \
     "3A | Setup Exim Mail Relay"  "Install Exim4 to relay mails via public email provider" \
     "3B | Setup Tailscale VPN"    "Establish or join a WireGuard based VPN using the Tailscale service" \
-=======
-    "35 | Serial port"            "Prepare serial ports for peripherals like Razberry, SCC, Pine64 ZWave, ..." \
-    "36 | WiFi setup"             "Configure wireless network connection" \
-    "   | Disable WiFi"           "Disable wireless network connection" \
-    "37 | Move root to USB"       "Move the system root from the SD card to a USB device (SSD or stick)" \
-    "38 | Use zram"               "Use compressed RAM/disk sync for active directories to avoid SD card corruption" \
-    "   | Uninstall zram"         "Don't use compressed memory (back to standard Raspberry Pi OS filesystem layout)" \
-    "39 | Setup Exim Mail Relay"  "Install Exim4 to relay mails via public email provider" \
-    "3A | Setup Tailscale VPN"    "Establish or join a WireGuard based VPN using the Tailscale service" \
->>>>>>> febbbeb5d (Update all documentation for clarity and accuracy (#1443))
     "   | Remove Tailscale VPN"   "Remove the Tailscale VPN service" \
     "   | Install WireGuard"      "Setup WireGuard to enable secure remote access to this system" \
     "   | Remove WireGuard"       "Remove WireGuard VPN from this system" \
@@ -209,17 +191,11 @@ show_main_menu() {
       36\ *) use_framebuffer "disable" ;;
       *Enable\ framebuffer) use_framebuffer "enable" ;;
       38\ *) init_zram_mounts "install" ;;
-<<<<<<< HEAD
       *Update\ zram) init_zram_mounts ;;
       *Uninstall\ zram) init_zram_mounts "uninstall" ;;
       39\ *) move_root2usb ;;
       3A\ *) exim_setup ;;
       3B\ *) if install_tailscale install; then setup_tailscale; fi;;
-=======
-      *Uninstall\ zram) init_zram_mounts "uninstall" ;;
-      39\ *) exim_setup ;;
-      3A\ *) if install_tailscale install; then setup_tailscale; fi;;
->>>>>>> febbbeb5d (Update all documentation for clarity and accuracy (#1443))
       *Remove\ Tailscale*) install_tailscale remove;;
       *Install\ WireGuard*) if install_wireguard install; then setup_wireguard; fi;;
       *Remove\ WireGuard*) install_wireguard remove;;
@@ -253,8 +229,8 @@ show_main_menu() {
     # shellcheck disable=SC2154
     case "$choice2" in
       41\ *) openhab_setup "release";;
-      *openHAB\ Milestone) cond_redirect apt-mark unhold openhab openhab-addons evcc && openhab_setup "milestone";;
-      *openHAB\ Snapshot) cond_redirect apt-mark unhold openhab openhab-addons evcc && openhab_setup "snapshot";;
+      *openHAB\ Milestone) openhab_setup "milestone";;
+      *openHAB\ Snapshot) openhab_setup "snapshot";;
       42\ *) openhab_shell_interfaces;;
       43\ *) openhab_clean_cache;;
       44\ *) nginx_setup;;
